dbc10a109559a3c12858f7b71d6b6f1868bed99d
L1ttleFinger <vincent.oluasa@gmail.com>
2023-11-11 18:53:27 +0100

    Add node number input

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import streamlit as st
 
-algorithm = st.sidebar.selectbox('Search Algorithm', ('Breadth-First Search', 'Depth-First Search', 'A*', 'Dijkstra'))
+min_nodes = 10
+max_nodes = 1000
+
+with st.sidebar:
+    algorithm = st.radio('Please select the desired search algorithm', ('Breadth-First Search', 'Depth-First Search', 'A*', 'Dijkstra'))
+    node_number = st.number_input(f'Please enter the desired amount of nodes, between {min_nodes} and {max_nodes}', min_value = min_nodes, max_value = max_nodes, value=100, step = 1)
 
 st.header('Graph Search Algorithms')
 st.divider()
 st.markdown(f'{algorithm} algorithm selected as search method')
+st.markdown(f'The graph contains {node_number} nodes')
 
 
