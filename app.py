77ddd9578eb51af5ec4c4292e7de49a05ab2f291
L1ttleFinger <vincent.oluasa@gmail.com>
2023-11-11 18:54:52 +0100

    Update streamlit interface

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ min_nodes = 10
 max_nodes = 1000
 
 with st.sidebar:
-    algorithm = st.radio('Please select the desired search algorithm', ('Breadth-First Search', 'Depth-First Search', 'A*', 'Dijkstra'))
     node_number = st.number_input(f'Please enter the desired amount of nodes, between {min_nodes} and {max_nodes}', min_value = min_nodes, max_value = max_nodes, value=100, step = 1)
+    st.divider()
+    algorithm = st.radio('Please select the desired search algorithm', ('Breadth-First Search', 'Depth-First Search', 'A*', 'Dijkstra'))
+
 
 st.header('Graph Search Algorithms')
 st.divider()
